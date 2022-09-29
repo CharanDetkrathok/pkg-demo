d3b1bbf6e3c9639aefed256bba90d350761b7313
Dr.ฮาริมดอย <d.charan8@Siripongs-MacBook-Air.local>
2022-09-29 16:01:01 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CharanDetkrathok/pkg-demo
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,13 @@
+package pkgdemo
+
+import "log"
+
+func LogInfo(message string) {
+	log.Printf("INFO - %v", message)
+}
+func LogWarning(message string) {
+	log.Printf("WARN - %v", message)
+}
+func LogError(message string) {
+	log.Printf("ERROR - %v", message)
+}
